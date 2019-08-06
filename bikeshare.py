7ccf821e03a6b7228cc2fedb68f5f37f22598f68
Kathrin Haeffs <kathrin@haeffs.de>
2019-08-06 13:02:36 +0200

    Add empty lines for better readability

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,9 @@ def get_filters():
         (str) month - number of the month to filter by, or 7 in case of no month filter
         (str) day - name of the day of week to filter by, or \'all\' to apply no day filter
     """
+
     print('Hello! Let\'s explore some US bikeshare data!')
+    
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         print('Would you like to see data for Chicago, New York City, or Washington?')
